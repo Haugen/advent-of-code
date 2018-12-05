2db69046838576019fb84bf353cc7cb05cc7d30f
Tobias Haugen <hej@tobiashaugen.se>
2018-12-05 17:33:22 -0500

    Day 1 part 1 complete.

---
+++ 1/part1/script.js
@@ -0,0 +1,23 @@
+const LineByLineReader = require('line-by-line');
+const lr = new LineByLineReader('./input.txt');
+
+const inputArray = [];
+let result = 0;
+
+lr.on('line', line => {
+  inputArray.push(line);
+});
+
+lr.on('end', () => {
+  for (let line of inputArray) {
+    const operator = line.charAt(0);
+    const value = Number(line.substring(1));
+    if (operator === '+') {
+      result += value;
+    } else {
+      result -= value;
+    }
+  }
+
+  console.log(result);
+});
